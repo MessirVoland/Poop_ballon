e931cf9a4b57bf890a20f4bbbfcc9e24081408ef
MessirVoland <30870916+MessirVoland@users.noreply.github.com>
2017-08-04 18:32:17 +0300

    Initial commit

---
+++ android/src/ru/asupd/poop_ballon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.asupd.poop_ballon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.asupd.poop_ballon.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
