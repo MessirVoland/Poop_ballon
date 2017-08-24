d97db397bd2ec25a545af817c60eb9919bcdae6e
MessirVoland <30870916+MessirVoland@users.noreply.github.com>
2017-08-24 13:09:59 +0300

    Эмн что то странное)

--- android/src/ru/asupd/poop_ballon/AndroidLauncher.java
+++ android/src/ru/asupd/poop_ballon/AndroidLauncher.java
@@ -1,71 +1,20 @@
 package ru.asupd.poop_ballon;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
-
 import ru.asupd.poop_ballon.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	public static final String TAG="AndroidLauncher";
-
-	protected AdView adView;
-
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		RelativeLayout layout = new RelativeLayout(this);
-
-		// Do the stuff that initialize() would do for you
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-
-
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
 		config.useAccelerometer = false;
 		config.useCompass = false;
-		View gameView = initializeForView(new MyGdxGame(), config);
-		// Add the libgdx view
-		layout.addView(gameView);
 
-		adView = new AdView(this);
-		adView.setAdListener(new AdListener() {
-			@Override
-			public void onAdLoaded() {
-				Log.i(TAG, "Ad Loaded...");
-			}
-		});
-		adView.setAdSize(AdSize.SMART_BANNER);
-		adView.setAdUnitId("ca-app-pub-6755493316893566/6656095586");
-
-		AdRequest.Builder builder = new AdRequest.Builder();
-		builder.addTestDevice("ADCD72548573E2D66A2AFC8594EDF6F6");
-		RelativeLayout.LayoutParams adParams = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT
-		);
-
-		adParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-		adParams.setMargins(0,0,0,50);
-		layout.addView(adView,adParams);
-		adView.loadAd(builder.build());
-
-		setContentView(layout);
+		initialize(new MyGdxGame(), config);
 	}
-
 }
