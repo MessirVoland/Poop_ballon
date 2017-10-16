fbef385ba8ba4b81ef591ee656a72f58268b493d
MessirVoland <mr.messir.voland@gmail.com>
2017-10-16 13:29:33 +0300

    большой взрыв с задержкой  #v.0.9.7.rev.B.build.30

--- core/src/ru/asupd/poop_ballon/Sprites/Bomb_balloon.java
+++ core/src/ru/asupd/poop_ballon/Sprites/Bomb_balloon.java
@@ -20,11 +20,13 @@ import static ru.asupd.poop_ballon.States.PlayState.make_poop_Sound;
  */
 
 public class Bomb_balloon extends Creature{
-    private int CHANSE_OF_SPAWN=2;//5%
+    private int CHANSE_OF_SPAWN=100;//5%
     private Vector3 velosity;
     private Sprite bomb_sprite;
     private boolean fly=false;
     private boolean left_side;
+    private boolean pooped;
+    private float current_delay;
     private int ballons_disposed;
     public Bomb_balloon() {
         bomb_sprite = new Sprite(Assets.instance.manager.get(Assets.bomb_balloon_t));
@@ -42,18 +44,25 @@ public class Bomb_balloon extends Creature{
             bomb_sprite.setPosition(480, 400 - 139 / 2);
             velosity = new Vector3(-120, 0, 0);
         }
+        pooped = false;
     }
 
     @Override
     public void update(float dt) {
         if (fly) {
-
             velosity.scl(dt);
             bomb_sprite.setPosition(bomb_sprite.getX() + velosity.x, bomb_sprite.getY() + velosity.y);
             velosity.scl(1 / dt);
             if (((!left_side)&(bomb_sprite.getX()<-105))|((left_side)&(bomb_sprite.getX()>480))){
                     restart();
                 }
+        }
+        if (pooped){
+            current_delay+=dt;
+            if (current_delay>=0.2f){
+                pooped=false;
+                setPooped();
+            }
         }
     }
     public void try_to_fly(){
@@ -71,32 +80,35 @@ public class Bomb_balloon extends Creature{
         if (fly){
             if (((y>bomb_sprite.getY())&(y< bomb_sprite.getY()+ bomb_sprite.getWidth()))&
             ((x>bomb_sprite.getX())&(x< bomb_sprite.getX()+ bomb_sprite.getHeight()))){
-                restart();
+
                 ballons_disposed=0;
                 combo_effects.add(new ParticleEffect(Assets.bomb_blow));
+                combo_effects.get(combo_effects.size - 1).setPosition(bomb_sprite.getX()+50,bomb_sprite.getY()+65);
                 combo_effects.get(combo_effects.size - 1).start();
-                combo_effects.get(combo_effects.size - 1).setPosition(bomb_sprite.getX(),bomb_sprite.getY());
-                for (Balloon balloon : balloons) {
-                    if (balloons_manager.wooden) {
-                        PlayState.score_num.addScore(balloons_manager.getCurrent_difficult_up());
-                        // System.out.println("Added 2 W score");
-                    } else {
-                        PlayState.score_num.addScore(1);
-                        //  System.out.println("Added 1 nW score");
-                    }
-                    if (!balloon.isPooped()) {
-                        ballons_disposed++;
-                    }
-                    balloon.setPooped();
-                    make_poop_Sound();
-
-
-                   // balloons.add(new Balloon(random(4) * 96, -195 - random(50), PlayState.get_speed_for_balloon(), !PlayState.boss_balloon.isStarted()));
-                }
-                for (int i=0;i<ballons_disposed;i++){
-                    balloons.add(new Balloon(random(4) * 96, -195 - random(50), PlayState.get_speed_for_balloon(), !PlayState.boss_balloon.isStarted()));
-                }
+                restart();
+                current_delay=0;
+                pooped=true;
             }
+        }
+    }
+    private void setPooped(){
+        for (Balloon balloon : balloons) {
+            if (balloons_manager.wooden) {
+                PlayState.score_num.addScore(balloons_manager.getCurrent_difficult_up());
+                // System.out.println("Added 2 W score");
+            } else {
+                PlayState.score_num.addScore(1);
+                //  System.out.println("Added 1 nW score");
+            }
+            if (!balloon.isPooped()) {
+                ballons_disposed++;
+            }
+            balloon.setPooped();
+            make_poop_Sound();
+            // balloons.add(new Balloon(random(4) * 96, -195 - random(50), PlayState.get_speed_for_balloon(), !PlayState.boss_balloon.isStarted()));
+        }
+        for (int i=0;i<ballons_disposed;i++){
+            balloons.add(new Balloon(random(4) * 96, -195 - random(50), PlayState.get_speed_for_balloon(), !PlayState.boss_balloon.isStarted()));
         }
     }
     private void restart(){
